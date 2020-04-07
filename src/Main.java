8b3613069cfacac1c1fc0e867b0fd5e434762c06
duygunuryldz <52247554+duygunuryldz@users.noreply.github.com>
2020-04-07 11:46:11 +0300

    Game is implemented in Main class.

--- src/Main.java
+++ src/Main.java
@@ -23,5 +23,26 @@ public class Main {
         result = arrorg.concatAndSort(list1, list2, list3);
 
         System.out.println("Result => " + result);
+
+        Player player1 = new Player("Tom");
+        Player player2 = new Player("John");
+        Player player3 = new Player("James");
+        Player player4 = new Player("Henry");
+
+        Die die = new Die();
+
+        for( int i = 0; i < 5; i++) {
+            player1.addScore(die.rollDie());
+            player2.addScore(die.rollDie());
+            player3.addScore(die.rollDie());
+            player4.addScore(die.rollDie());
+        }
+
+        System.out.println("-------------------");
+        System.out.println(player1);
+        System.out.println(player2);
+        System.out.println(player3);
+        System.out.println(player4);
+
     }
 }
